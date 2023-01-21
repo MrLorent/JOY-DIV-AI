e6dc5135bd563b147463d2b48f60afafa12e4295
Mr Lorent <lorent.tanguy@gmail.com>
2023-01-21 17:48:43 +0100

    🔧 [frontend] setup tailwindcss

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,5 +1,12 @@
+// STYLES
+import "./index.css";
+
+
+// LIBRARIES
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+
+// COMPONENTS
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
---
+++ frontend/tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    /*======== COLORS ========*/
+    colors: {
+      background: "var(--black)",
+      primary:    "var(--white)",
+    },
+    extend: {},
+  },
+  plugins: [],
+}
