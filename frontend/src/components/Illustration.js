9275d0be298484fad4c729507e43be68f9980a6c
Mr Lorent <lorent.tanguy@gmail.com>
2023-02-13 01:21:47 +0100

    ✨ [Frontend] Fix Illustraction component refactor

--- frontend/src/components/Illustration.js
+++ frontend/src/components/Illustration.js
@@ -9,26 +9,32 @@ const Illustration = ({ curves, loading }) => {
             <span className="mb-3">The Illustration :</span>
             <div id="illustration" className="w-full h-full flex flex-col items-center overflow-x-hidden overflow-y-auto pt-28 p-5 border border-tertiary rounded-lg relative">
             {
-                curves ?
-                    curves.map((curve, index) => <div key={index} className={"w-full h-5 flex justify-center items-end"} dangerouslySetInnerHTML={{__html: curve}}></div>)
-                    &&
-                    (loading ?
-                        <div className="flex">
-                            <span>Processing next line</span>
-                            <span className="one-dot">.</span>
-                            <span className="two-dots">.</span>
-                            <span className="three-dots">.</span>
-                        </div>
-                    :
-                        null)
-                : 
-                    (loading ?
+                curves ? (
+                    <>
+                    {
+                        curves.map((curve, index) => <div key={index} className={"w-full h-5 flex justify-center items-end"} dangerouslySetInnerHTML={{__html: curve}}></div>)
+                    }
+                    {
+                        loading ?
+                            <div className="flex">
+                                <span>Processing next line</span>
+                                <span className="one-dot">.</span>
+                                <span className="two-dots">.</span>
+                                <span className="three-dots">.</span>
+                            </div>
+                        :
+                            null
+                    }
+                    </>
+                ) : (
+                    loading ?
                         <Loader />
                     :
                         <div className="w-fit h-fit flex flex-col items-center pb-28 m-auto">
                             <span>No poem to illustrate yet...</span>
                             <span className="m-3">［(－－)］zzzz</span>
-                        </div>)
+                        </div>
+                )
             }
             </div>
         </div>
