8a1b8c15f8f7b7b478ab7007b44c8f4efd268f2b
Mr Lorent <lorent.tanguy@gmail.com>
2023-02-13 04:20:03 +0100

    ✨ [Frontend][Loader] Set the loader to an old school one

--- frontend/src/components/Illustration.js
+++ frontend/src/components/Illustration.js
@@ -18,9 +18,11 @@ const Illustration = ({ curves, loading }) => {
                         loading ?
                             <div className="flex">
                                 <span>Processing next line</span>
-                                <span className="one-dot">.</span>
-                                <span className="two-dots">.</span>
-                                <span className="three-dots">.</span>
+                                <div className="w-12 h-fit flex">
+                                    <span className="one-dot text-base">.</span>
+                                    <span className="two-dots text-base">.</span>
+                                    <span className="three-dots text-base">.</span>
+                                </div>
                             </div>
                         :
                             null
--- frontend/src/components/Loader.js
+++ frontend/src/components/Loader.js
@@ -1,24 +1,14 @@
 const Loader = () => {
     return (
       <div className="bg-background w-full h-full flex justify-center items-center absolute top-0 right-0 z-1000">
-        <svg width="57" height="57" viewBox="0 0 57 57" className="stroke-1 stroke-primary">
-            <g fill="none" fillRule="evenodd">
-                <g transform="translate(1 1)">
-                    <circle cx="5" cy="50" r="5">
-                        <animate attributeName="cy" begin="0s" dur="2.2s" values="50;5;50;50" calcMode="linear" repeatCount="indefinite"/>
-                        <animate attributeName="cx" begin="0s" dur="2.2s" values="5;27;49;5" calcMode="linear" repeatCount="indefinite"/>
-                    </circle>
-                    <circle cx="27" cy="5" r="5">
-                        <animate attributeName="cy" begin="0s" dur="2.2s" from="5" to="5" values="5;50;50;5" calcMode="linear" repeatCount="indefinite"/>
-                        <animate attributeName="cx" begin="0s" dur="2.2s" from="27" to="27" values="27;49;5;27" calcMode="linear" repeatCount="indefinite"/>
-                    </circle>
-                    <circle cx="49" cy="50" r="5">
-                        <animate attributeName="cy" begin="0s" dur="2.2s" values="50;50;5;50" calcMode="linear" repeatCount="indefinite"/>
-                        <animate attributeName="cx" from="49" to="49" begin="0s" dur="2.2s" values="49;5;27;49" calcMode="linear" repeatCount="indefinite"/>
-                    </circle>
-                </g>
-            </g>
-        </svg>
+        <div className="flex">
+            <span>Loading</span>
+            <div className="w-12 h-fit flex">
+                <span className="one-dot text-base">.</span>
+                <span className="two-dots text-base">.</span>
+                <span className="three-dots text-base">.</span>
+            </div>
+        </div>
       </div>
     );
   };
